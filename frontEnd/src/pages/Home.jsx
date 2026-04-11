9dc1c8e4584b98e5a4b4e323924e7e9460d9d297
Luiz Gustavo <luizg6oliveira@gmail.com>
2026-04-11 00:25:21 -0300

    UX pagina home, terminado

--- frontEnd/src/pages/Home.jsx
+++ frontEnd/src/pages/Home.jsx
@@ -5,84 +5,95 @@ function Home() {
     const navigate = useNavigate();
 
     return (
+  <div className='container'>
     <div>
-        <div className='cabecalho'>
-            <h1>Bem vindo ao <span className="nome">Mood Music</span></h1>
-            <h2>Deixe a música entender o que você sente</h2>
-        </div>
+  <div className='cabecalho'>
+    <h1 className='titulo'>Mood Music</h1>
+    <h2 className='subtitulo'>Deixe a música entender o que você sente</h2>
+  </div>
 
-<div className="texto1">
-  <h3>Sobre sentir, não só ouvir</h3>
+  <div className="texto1">
+    <h3>Sobre sentir, não só ouvir.</h3>
 
-  <p className="bloco">
-    A música sempre foi algo muito importante na minha vida.
-    <br />
-    Ela esteve presente nos meus melhores momentos...
-    <br />
-    e também nos mais difíceis.
-  </p>
+    <div className='blocos-topo'>
+      <div className='coluna'>
+        <p className="bloco destaque1">
+          A música sempre foi algo muito importante na minha vida.
+          <br />
+          Ela esteve presente nos meus melhores momentos...
+          <br />
+          e também nos mais difíceis.
+        </p>
 
-  <p className="bloco">
-    E com o tempo eu percebi uma coisa:
-    <br />
-    A música não muda só o seu humor.
-    <br />
-    Ela te acompanha nele.
-  </p>
+        <p className="bloco nao-destaque1">
+          E é exatamente isso que a música faz.
+          <br />
+          Ela te conforta na emoção que você está sentindo,
+          <br />
+          e, aos poucos, te dá espaço pra entender aquilo...
+          <br />
+          e até sair desse estado, se quiser.
+        </p>
+      </div>
 
-  <p className="bloco destaque">
-    Um exemplo:
-    <br />
-    se você está triste, você não quer ouvir algo barulhento ou fora do momento...
-    <br />
-    você quer algo que te entenda, algo confortável.
-  </p>
+      <div className='coluna'>
+        <p className="bloco destaque">
+          E com o tempo eu percebi uma coisa:
+          <br />
+          A música não muda só o seu humor.
+          <br />
+          Ela te acompanha nele.
+        </p>
 
-  <p className="bloco">
-    E é exatamente isso que a música faz.
-    <br />
-    Ela te conforta na emoção que você está sentindo,
-    <br />
-    e, aos poucos, te dá espaço pra entender aquilo...
-    <br />
-    e até sair desse estado, se quiser.
-  </p>
+        <p className="bloco nao-destaque">
+          E foi pensando nisso que esse projeto nasceu...
+          <br />
+          O Mood Music não foi criado só para indicar músicas.
+          <br />
+          Ele foi criado para entender momentos.
+          <br />
+          Para entregar sons que combinem com o que você sente agora.
+        </p>
+      </div>
 
-  <p className="bloco destaque">
-    E foi pensando nisso que esse projeto nasceu...
-    <br />
-    O Mood Music não foi criado só para indicar músicas.
-    <br />
-    Ele foi criado para entender momentos.
-    <br />
-    Para entregar sons que combinem com o que você sente agora.
-  </p>
+      <div className='coluna'>
+        <p className="bloco destaque3">
+          Um exemplo:
+          <br />
+          se você está triste, você não quer ouvir algo barulhento ou fora do momento...
+          <br />
+          você quer algo que te entenda, algo confortável.
+        </p>
 
-  <p className="bloco">
-    Porque às vezes tudo o que a gente precisa
-    <br />
-    não é de alguém dizendo o que fazer...
-    <br />
-    mas de uma música certa, no momento certo.
-  </p>
+        <p className="bloco nao-destaque3">
+          Porque às vezes tudo o que a gente precisa
+          <br />
+          não é de alguém dizendo o que fazer...
+          <br />
+          mas de uma música certa, no momento certo.
+        </p>
+      </div>
+    </div>
 
-  <p className="bloco destaque">
-    Uma música que acolhe.
-    <br />
-    Uma música que conversa com você sem dizer uma palavra.
-    <br />
-    Uma música que transforma o ambiente, a cabeça e o coração.
-  </p>
-
-  <p className="bloco final">
-    Esse projeto é sobre isso.
-    <br />
-    Sobre emoção.
-    <br />
-    Sobre conexão.
-    <br />
-    Sobre encontrar, na música, um lugar onde você possa se sentir entendido.
-  </p>
+    <div className='blocos-finais'>
+      <p className="bloco destaqueM">
+        Uma música que acolhe.
+        <br />
+        Uma música que conversa com você sem dizer uma palavra.
+        <br />
+        Uma música que transforma o ambiente, a cabeça e o coração.
+      </p>
+    </div>
+  </div>
+      <p className="bloco final">
+        Esse projeto é sobre isso.
+        <br />
+        Sobre emoção.
+        <br />
+        Sobre conexão.
+        <br />
+        Sobre encontrar, na música, um lugar onde você possa se sentir entendido.
+      </p>
 </div>
 
         <div className='escolhas'>
