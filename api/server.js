c2b2ce480fe162f49598631dfa7815ecdd4b9347
Luiz Gustavo <luizg6oliveira@gmail.com>
2026-04-12 00:18:32 -0300

    mudança no SELECT

--- api/server.js
+++ api/server.js
@@ -44,7 +44,7 @@ app.get('/playlist/:emocao/:tempo', async (req, res) => {
             return res.status(400).json({error: 'Tempo Invalido'});
         }
 
-        const[musicas] = await pool.query('SELECT * FROM musicas WHERE emocao_principal = ? ORDER BY intensidade DESC, rand();', [emocao]);
+        const[musicas] = await pool.query('SELECT * FROM musicas WHERE emocao_principal = ? ORDER BY rand();', [emocao]);
 
         let playlist = [];
         let total = 0;
