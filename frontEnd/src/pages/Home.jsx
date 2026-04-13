2f1de42621c5aa5403513fb03d8de24841cd94d0
Luiz Gustavo <luizg6oliveira@gmail.com>
2026-04-12 23:42:52 -0300

    adicionado dinamica de interface para mobile

--- frontEnd/src/pages/Home.jsx
+++ frontEnd/src/pages/Home.jsx
@@ -13,78 +13,132 @@ function Home() {
   </div>
 
   <div className="texto1">
-    <h3>Sobre sentir, não só ouvir.</h3>
+  <h3>Sobre sentir, não só ouvir.</h3>
 
-    <div className='blocos-topo'>
-      <div className='coluna'>
-        <p className="bloco destaque1">
-          A música sempre foi algo muito importante na minha vida.
-          <br />
-          Ela esteve presente nos meus melhores momentos...
-          <br />
-          e também nos mais difíceis.
-        </p>
+  <div className="blocos-topo desktop-only">
+    <div className="coluna">
+      <p className="bloco destaque1">
+        A música sempre foi algo muito importante na minha vida.
+        <br />
+        Ela esteve presente nos meus melhores momentos...
+        <br />
+        e também nos mais difíceis.
+      </p>
 
-        <p className="bloco nao-destaque1">
-          E é exatamente isso que a música faz.
-          <br />
-          Ela te conforta na emoção que você está sentindo,
-          <br />
-          e, aos poucos, te dá espaço pra entender aquilo...
-          <br />
-          e até sair desse estado, se quiser.
-        </p>
-      </div>
-
-      <div className='coluna'>
-        <p className="bloco destaque">
-          E com o tempo eu percebi uma coisa:
-          <br />
-          A música não muda só o seu humor.
-          <br />
-          Ela te acompanha nele.
-        </p>
-
-        <p className="bloco nao-destaque">
-          E foi pensando nisso que esse projeto nasceu...
-          <br />
-          O Mood Music não foi criado só para indicar músicas.
-          <br />
-          Ele foi criado para entender momentos.
-          <br />
-          Para entregar sons que combinem com o que você sente agora.
-        </p>
-      </div>
-
-      <div className='coluna'>
-        <p className="bloco destaque3">
-          Um exemplo:
-          <br />
-          se você está triste, você não quer ouvir algo barulhento ou fora do momento...
-          <br />
-          você quer algo que te entenda, algo confortável.
-        </p>
-
-        <p className="bloco nao-destaque3">
-          Porque às vezes tudo o que a gente precisa
-          <br />
-          não é de alguém dizendo o que fazer...
-          <br />
-          mas de uma música certa, no momento certo.
-        </p>
-      </div>
+      <p className="bloco nao-destaque1">
+        E é exatamente isso que a música faz.
+        <br />
+        Ela te conforta na emoção que você está sentindo,
+        <br />
+        e, aos poucos, te dá espaço pra entender aquilo...
+        <br />
+        e até sair desse estado, se quiser.
+      </p>
     </div>
 
-    <div className='blocos-finais'>
-      <p className="bloco destaqueM">
-        Uma música que acolhe.
+    <div className="coluna">
+      <p className="bloco destaque">
+        E com o tempo eu percebi uma coisa:
         <br />
-        Uma música que conversa com você sem dizer uma palavra.
+        A música não muda só o seu humor.
         <br />
-        Uma música que transforma o ambiente, a cabeça e o coração.
+        Ela te acompanha nele.
+      </p>
+
+      <p className="bloco nao-destaque">
+        E foi pensando nisso que esse projeto nasceu...
+        <br />
+        O Mood Music não foi criado só para indicar músicas.
+        <br />
+        Ele foi criado para entender momentos.
+        <br />
+        Para entregar sons que combinem com o que você sente agora.
+      </p>
+    </div>
+
+    <div className="coluna">
+      <p className="bloco destaque3">
+        Um exemplo:
+        <br />
+        se você está triste, você não quer ouvir algo barulhento ou fora do momento...
+        <br />
+        você quer algo que te entenda, algo confortável.
+      </p>
+
+      <p className="bloco nao-destaque3">
+        Porque às vezes tudo o que a gente precisa
+        <br />
+        não é de alguém dizendo o que fazer...
+        <br />
+        mas de uma música certa, no momento certo.
       </p>
     </div>
   </div>
+
+  <div className="blocos-topo-mobile mobile-only">
+    <p className="bloco destaque1">
+      A música sempre foi algo muito importante na minha vida.
+      <br />
+      Ela esteve presente nos meus melhores momentos...
+      <br />
+      e também nos mais difíceis.
+    </p>
+
+    <p className="bloco destaque">
+      E com o tempo eu percebi uma coisa:
+      <br />
+      A música não muda só o seu humor.
+      <br />
+      Ela te acompanha nele.
+    </p>
+
+    <p className="bloco nao-destaque1">
+      E é exatamente isso que a música faz.
+      <br />
+      Ela te conforta na emoção que você está sentindo,
+      <br />
+      e, aos poucos, te dá espaço pra entender aquilo...
+      <br />
+      e até sair desse estado, se quiser.
+    </p>
+
+    <p className="bloco destaque3">
+      Um exemplo:
+      <br />
+      se você está triste, você não quer ouvir algo barulhento ou fora do momento...
+      <br />
+      você quer algo que te entenda, algo confortável.
+    </p>
+
+    <p className="bloco nao-destaque">
+      E foi pensando nisso que esse projeto nasceu...
+      <br />
+      O Mood Music não foi criado só para indicar músicas.
+      <br />
+      Ele foi criado para entender momentos.
+      <br />
+      Para entregar sons que combinem com o que você sente agora.
+    </p>
+
+    <p className="bloco nao-destaque3">
+      Porque às vezes tudo o que a gente precisa
+      <br />
+      não é de alguém dizendo o que fazer...
+      <br />
+      mas de uma música certa, no momento certo.
+    </p>
+  </div>
+
+  <div className="blocos-finais">
+    <p className="bloco destaqueM">
+      Uma música que acolhe.
+      <br />
+      Uma música que conversa com você sem dizer uma palavra.
+      <br />
+      Uma música que transforma o ambiente, a cabeça e o coração.
+    </p>
+  </div>
+</div>
       <p className="bloco final">
         Esse projeto é sobre isso.
         <br />
@@ -107,7 +161,7 @@ function Home() {
             <button className='btn-alegria' onClick={() => navigate("/playlist/alegria")}>Alegria</button>
             <button className='btn-tristeza' onClick={() => navigate("/playlist/tristeza")}>Tristeza</button>
             <button className='btn-raiva' onClick={() => navigate("/playlist/raiva")}>Raiva</button>
-            <button className='btn-apaixonado' onClick={() => navigate("playlist/apaixonado")}>Apaixonado</button>
+            <button className='btn-apaixonado' onClick={() => navigate("/playlist/apaixonado")}>Apaixonado</button>
             <button className='btn-confuso' onClick={() => navigate("/playlist/confuso")}>Confuso</button>
             <button className='btn-tranquilo' onClick={() => navigate("/playlist/tranquilo")}>Tranquilo</button>
         </div>
